07db02c77415acb752f76824d392c702087ea3c5
carlosv <409925519@qq.com>
2020-08-04 10:41:38 +0800

    clear node data

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,36 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions
-    const result = await graphql(`
-      query {
-        allNavYaml {
-            edges {
-              node {
-                label
-                title
-                description
-                items {
-                  label
-                  title
-                }
-              }
-            }
-          }
-      }
-    `)
-    result.data.allNavYaml.edges.forEach(({ node }) => {
-      console.log(node)
-      createPage({
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.,
-          label: node.label,
-          title: node.title,
-          description: node.description
-        },
-      })
-    })
-  }
